743c7aeb78f76db727a9cb84f641713907aeae92
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-02-22 11:32:39 +0700

    # feat AcceptJson and AddToken middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AcceptJson;
+use App\Http\Middleware\AddToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->append(AcceptJson::class);
+        $middleware->append(AddToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
