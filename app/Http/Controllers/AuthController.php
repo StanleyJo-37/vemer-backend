db52f44774698be22781cf1176391301b17a5c07
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-03-26 11:42:24 +0700

    # feat socialite config

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use App\Http\Resources\UserResource;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
@@ -87,6 +89,75 @@ class AuthController extends Controller
                     ->withCookie($cookie);
         }
         catch (Exception $e) {
+            return response()->json(
+                [
+                    'message' => 'Error logging in. Please try again later.',
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function loginSSO(Request $request) {
+        try {
+            $request->validate([
+                'provider' => 'string|required|in:google,facebook,x,linkedin-openid,github,gitlab,bitbucket,slack,slack-openid',
+                'web_origin' => 'string',
+                'target_path' => 'string',
+            ]);
+
+            $oauthUrl = Socialite::driver($request->provider)->stateless()->redirect()->getTargetUrl();
+
+            return response()->json([
+                'redirect_url' => $oauthUrl . "&state=" . urlencode("web_origin=$request->web_origin&target_path=$request->target_path"),
+            ]);
+        } catch (Exception $e) {
+            // DB::rollBack();
+            return response()->json(
+                [
+                    'message' => 'Error logging in. Please try again later.',
+                    'error' => $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
+
+    public function callbackSSO(string $provider, Request $request) {
+        try {
+            DB::beginTransaction();
+
+            if (! in_array($provider, ['google', 'facebook', 'x', 'linkedin-openid', 'github', 'gitlab', 'bitbucket', 'slack', 'slack-openid'])) {
+                return response()->json("Provider not found.", 403);
+            }
+
+            $socialUser = Socialite::driver($provider)->stateless()->user();
+
+            $user = User::updateOrCreate(
+                ['email' => $socialUser->getEmail()],  // Search by email
+                [
+                    'name' => $socialUser->getName(),
+                    'email_verified_at' => now(), // Mark email as verified
+                    'password' => bcrypt(uniqid()), // Random password (not used)
+                    'avatar' => $socialUser->getAvatar(), // If your model has an avatar field
+                    'provider' => $request->provider,
+                    'provider_id' => $socialUser->getId(),
+                ]
+            );
+
+            $cookie = $this->createToken($user);
+
+            DB::commit();
+
+            return response()
+                    ->json([
+                        'user' => new UserResource($user),
+                        'message' => 'Login Successful.',
+                    ])
+                    ->withCookie($cookie);
+        } catch (Exception $e) {
+            DB::rollBack();
             return response()->json(
                 [
                     'message' => 'Error logging in. Please try again later.',
---
+++ routes/v1/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Public
+Route::prefix('/public')->group(function () {
+    Route::prefix('/auth')->group(function() {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::prefix('/login')->group(function() {
+            Route::post('/', [AuthController::class, 'login']);
+            Route::post('/sso', [AuthController::class, 'loginSSO']);
+            Route::post('/sso/callback/{provider}', [AuthController::class, 'callbackSSO']);
+        });
+    });
+});
+
+// Auth
+Route::prefix('/auth')->middleware('auth:sanctum')->group(function () {
+    Route::prefix('/activities')->group(function () {
+        Route::get('/', [ActivityController::class, 'get']);
+        Route::get('/{id}', [ActivityController::class, 'getDetail']);
+    });
+});
