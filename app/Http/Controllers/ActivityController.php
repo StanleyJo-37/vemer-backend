3d78b5b4b3144a3ffb119ccb146156fe21399ee7
123jason689 <jason.melvin689@gmail.com>
2025-05-13 22:57:59 +0700

    # Added activity controller, added function to get activity by id

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Models\Activity;
+
+
+class ActivityController extends Controller
+{
+    public function getActivityById(Request $request, int $id)
+    {
+        $activity = Activity::find($id);
+
+        if($activity == NULL){
+            return response()->json([
+                'message' => 'Activity not found !',
+                'error' => true
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Activity found successfully',
+            'error' => false,
+            'item' => $activity
+        ]);
+    }
+}
