b44744960c09b0431005ebfc0686cf00cf86568d
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-06 12:15:39 +0700

    # fix sso callback

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -13,7 +13,7 @@ Route::prefix('/public')->group(function () {
         Route::prefix('/login')->group(function() {
             Route::post('/', [AuthController::class, 'login']);
             Route::post('/sso', [AuthController::class, 'loginSSO']);
-            Route::post('/sso/callback/{provider}', [AuthController::class, 'callbackSSO']);
+            Route::get('/sso/callback/{provider}', [AuthController::class, 'callbackSSO']);
         });
     });
 });
