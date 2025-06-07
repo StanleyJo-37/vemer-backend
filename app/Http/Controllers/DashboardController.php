9a8785b1aa69d3e844b6591b289e67c48022a35d
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-06-07 11:23:01 +0700

    # feat generalStats fetching

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    //
+    public function getGeneralStats(Request $request) {
+        try {
+            $user = Auth::user();
+
+            $stats = DB::table('users as u')
+                        ->select([
+                            DB::raw('COALESCE(SUM(up.point), 0) AS "totalPoints"'),
+                            DB::raw("(
+                                SELECT COALESCE(COUNT(ap.id), 0) FROM activity_participants ap WHERE ap.user_id = $user->id
+                            ) AS \"eventsParticipated\""),
+                            DB::raw("(
+                                SELECT COALESCE(COUNT(ub.id), 0) FROM user_badges ub WHERE ub.user_id = $user->id
+                            ) AS \"badgesEarned\""),
+                        ])
+                        ->join('user_points as up', 'up.user_id', '=','u.id')
+                        ->where('u.id', $user->id)
+                        ->first();
+
+            return response()->json($stats);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
